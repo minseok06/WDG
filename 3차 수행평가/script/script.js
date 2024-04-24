b5be813ce6b0bc07b72e3a9aaa32ab348697c8fc
양민석 | Yang Minseok <121544294+minseok06@users.noreply.github.com>
2024-04-24 12:42:59 +0900

    Delete 3차 수행평가 directory

--- 3차 수행평가/script/script.js
+++
@@ -1,6 +0,0 @@
-$('.main > li').mouseover(function(){
-    $(this).children('.sub').stop().slideDown();
-});
-$('.main > li').mouseout(function(){
-    $(this).children('.sub').stop().slideUp();
-});
