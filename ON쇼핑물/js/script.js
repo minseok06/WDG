098e988e4ca116e66a012091d24d5d4872418c39
양민석 | Yang Minseok <121544294+minseok06@users.noreply.github.com>
2024-05-08 10:45:02 +0900

    0508

--- ON쇼핑물/js/script.js
+++ ON쇼핑물/js/script.js
@@ -1,6 +1,32 @@
+// 슬라이드 다운 슬라이드 업
 $('.navi > li').mouseover(function(){
     $(this).children('.sub').stop().slideDown();
 });
 $('.navi > li').mouseout(function(){
     $(this).children('.sub').stop().slideUp();
+});
+
+// 페이드 인 페이드 아웃
+$('.imgslide a:gt(0)').hide();
+setInterval(function(){
+    $('.imgslide a:first-child').fadeOut()
+    .next('a').fadeIn()
+    .end().appendTo('.imgslide');
+}, 3000);
+
+// 공지사항 갤러리 탭 메뉴
+$(function(){
+    $('.tabmenu>li>a').click(function(){
+        $(this).parent().addClass('active')
+         .siblings().removeClass('active');
+        return false;
+    });
+});
+
+// 팝업창
+$('.c1 li:first').click(function(){
+    $('#modal').addClass('active');
+});
+$('.btn').click(function(){
+    $('#modal').removeClass('active');
 });
